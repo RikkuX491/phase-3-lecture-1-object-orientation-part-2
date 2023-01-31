96cb5a803542c10b24b4821cbfa910efebc66442
Ricardo Guerra <RikkuG491@gmail.com>
2023-01-31 02:44:06 -0500

    Created lecture repository

---
+++ animal.rb
@@ -0,0 +1,33 @@
+class Animal
+
+    def initialize(name, breed, age)
+        @name = name
+        @breed = breed
+        @age = age
+    end
+
+    def name
+        @name
+    end
+
+    def name=(name)
+        @name = name
+    end
+
+    def breed
+        @breed
+    end
+
+    def breed=(breed)
+        @breed = breed
+    end
+
+    def age
+        @age
+    end
+
+    def age=(age)
+        @age = age
+    end
+
+end
